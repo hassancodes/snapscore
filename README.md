05010f831731878c62aa22dae6853f94d1f29d0a
Muhammad hassan <hassanhanjra900@gmail.com>
2022-10-21 10:34:43 -0400

    feat: sending concurrent snaps

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # snapscore
 An adb bot that builds my snapscore automatically
 # Flex while you unsnap 😂
+
+# Implementing multiple snapsends concurrently.
+Efficiency will be increased by 500% assuming that we are sending snaps to 5 people at the same time.
+
--- snapscore.py
+++ snapscore.py
@@ -11,27 +11,26 @@ class SnapScore:  # cam image and screen image should be string
         self.device = device
         # self.cam_img = cam_img
         # self.screen_img = screen_img
-    
-    
+
     def click_camera(self):
-        self.device.shell(f'input touchscreen tap {cam_points[0]} {cam_points[1]}')
+        self.device.shell(
+            f'input touchscreen tap {cam_points[0]} {cam_points[1]}')
         time.sleep(1)
-        
+
     def take_picture(self):
-        self.device.shell(f'input touchscreen tap {cam_click_points[0]} {cam_click_points[1]}')
+        self.device.shell(
+            f'input touchscreen tap {cam_click_points[0]} {cam_click_points[1]}')
         time.sleep(1)
-    
+
     def send_image(self):
-        self.device.shell(f'input touchscreen tap {send_cord[0]} {send_cord[1]}')
+        self.device.shell(
+            f'input touchscreen tap {send_cord[0]} {send_cord[1]}')
         time.sleep(1)
-    
 
 
-
-    
-cam_points = (72,1582)
-cam_click_points =(535,2055)
-send_cord = (995,2280)
+cam_points = (72, 1582)
+cam_click_points = (535, 2055)
+send_cord = (995, 2280)
 
 
 client = AdbClient(host="127.0.0.1", port=5037)
@@ -45,13 +44,28 @@ device = devices[0]
 
 
 snp = SnapScore(device)
-if __name__ =="__main__":
-    snapsent = 0
+if __name__ == "__main__":
+    cam_points = (72, 1582)
+    cam_click_points = (535, 2055)
+    send_cord = (995, 2280)
+    client = AdbClient(host="127.0.0.1", port=5037)
+    devices = client.devices()
     
-    while(snapsent!=1000):
-        snapsent+=1
-        print(f"total Number of snap sent are",snapsent)
-        snp.click_camera()
-        snp.take_picture()
-        snp.send_image()    
-        
+    if len(devices) == 0:
+        print("No devices attached")
+        quit()
+    device = devices[0]
+
+    if sys.argv[1] == 'exp':
+        # write exponential code over here
+        pass
+    else:
+
+        snapsent = 0
+        while(snapsent !=1000):
+            print("lol")
+            snapsent += 1
+            print(f"total Number of snap sent are", snapsent)
+            snp.click_camera()
+            snp.take_picture()
+            snp.send_image()
